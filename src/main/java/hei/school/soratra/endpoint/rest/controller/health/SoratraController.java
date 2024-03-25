ae751538075f4661002c91bc553cabbe9f535163
Faniry <faniryrandria08@gmail.com>
2024-03-25 09:39:47 +0300

    feat: modify soratra controller

--- src/main/java/hei/school/soratra/endpoint/rest/controller/health/SoratraController.java
+++ src/main/java/hei/school/soratra/endpoint/rest/controller/health/SoratraController.java
@@ -1,32 +1,31 @@
 package hei.school.soratra.endpoint.rest.controller.health;
 
-import hei.school.soratra.PojaGenerated;
+import java.util.Locale;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Locale;
-
 @RestController
 public class SoratraController {
 
-    @PutMapping("/soratra/{id}")
-    public ResponseEntity<?> putSoratra(
-            @PathVariable("id") int id,
-            @RequestBody PoeticPhraseRequest request) {
-        return ResponseEntity.ok().build();
-    }
-    static class PoeticPhraseRequest {
-        private String phrase_poetique;
+  @PutMapping("/soratra/{id}")
+  public ResponseEntity<?> putSoratra(
+      @PathVariable("id") int id, @RequestBody PoeticPhraseRequest request) {
+    return ResponseEntity.ok().build();
+  }
 
-        public String getPhrase_poetique() {
-            return phrase_poetique;
-        }
 
-        public void setPhrase_poetique(String phrase_poetique) {
-            this.phrase_poetique = phrase_poetique.toLowerCase(Locale.ROOT);
-        }
+  static class PoeticPhraseRequest {
+    private String phrase_poetique;
+
+    public String getPhrase_poetique() {
+      return phrase_poetique;
     }
+
+    public void setPhrase_poetique(String phrase_poetique) {
+      this.phrase_poetique = phrase_poetique.toLowerCase(Locale.ROOT);
+    }
+  }
 }
